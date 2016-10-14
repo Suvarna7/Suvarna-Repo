0889f4ace5a6b23d9e737a6f2d052f344a4b48fb
Suvarna7 <sjulania@hawk.iit.edu>
2016-10-14 15:08:52 +0000

    Positional parameters addition to create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-echo "Creating load balancer"
-aws elb create-load-balancer --load-balancer-name ITMO544sj --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets subnet-5e511128 --security-groups sg-a6b560df
-echo "Creating launch configuration"
-aws autoscaling create-launch-configuration --launch-configuration-name itmo544sjconfigure --image-id ami-06b94666 --security-group sg-a6b560df --key-name insomnia --instance-type t2.micro --user-data file://installenv.sh
-echo "Creating auto scaling group to launch intances"
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name server-rg --launch-configuration itmo544sjconfigure --availability-zone us-west-2b --load-balancer-name ITMO544sj --max-size 5 --min-size 2 --desired-capacity 3
 
+if [ "$#" -ne 5 ]; then
+    echo "Since all 5 parameters are not present script terminated"
+   exit 0
+else 
+ echo "Value of AMI ID=" $1
+ echo "Value of key-name=" $2
+ echo "Value of security-group=" $3
+ echo "Value of launch-configuration=" $4
+ echo "Value of count="$5
+fi
+ 
+echo "Creating load balancer"
+aws elb create-load-balancer --load-balancer-name ITMO544sj --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets subnet-5e511128 --security-groups $3
+
+echo "Creating launch configuration"
+
+aws autoscaling create-launch-configuration --launch-configuration-name $4 --image-id $1 --security-group $3 --key-name $2 --instance-type t2.micro --user-data file://installenv.sh
+echo "Creating auto scaling group to launch intances"
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name server-rg --launch-configuration $4 --availability-zone us-west-2b --load-balancer-name ITMO544sj --max-size 5 --min-size 2 --desired-capacity $5
